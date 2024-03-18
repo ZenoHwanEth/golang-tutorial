85dfc24527aa7a8c70984dad876e163e4ada0f9f
ZenoHwanEth <hwan2221@hotmail.com>
2024-03-18 23:56:23 +0800

    adding why use golang file

---
+++ 1.whyusegolang.go
@@ -0,0 +1,16 @@
+package main
+
+func whysUseGolang() {
+	/*
+		# means important
+		1. General purpose language
+		2. Great backend language
+		3. Excels at Concurrency #
+		4. Easy to understand #
+		5. many built-in features
+		6. strongly typed #
+		7. memory freed automatically #
+		8. fast compile time #
+		9. cross platform support
+	*/
+}
