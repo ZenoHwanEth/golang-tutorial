87f03427c0df22b8608daac209ef0f068d7fc0d0
ZenoHwanEth <hwan2221@hotmail.com>
2024-03-20 00:02:21 +0800

    adding changes in hello.go

--- 2.hello.go
+++ 2.hello.go
@@ -4,22 +4,42 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
+	"reflect"
 )
 
 var p1 = fmt.Println
 
 func main() {
-	p1("what is ur name")
-	reader := bufio.NewReader(os.Stdin)
-	name, err := reader.ReadString('\n')
+	// 1.
+	// p1("what is ur name")
+	// reader := bufio.NewReader(os.Stdin)
+	// name, err := reader.ReadString('\n')
 
-	if err == nil {
-		p1("Hello", name)
-	} else {
-		log.Fatal(err)
-	}
+	// if err == nil {
+	// 	p1("Hello", name)
+	// } else {
+	// 	log.Fatal(err)
+	// }
+
+	// 2. variable
+	// var name type
+	// var vName string = "Derek"
+	// var v1, v2 = 1.2, 3.4
+	// var v3 = "Hello"
+	// // variable is mutable
+	// v4 := 2.4 // auto define data type
+	// v4 = 5.4
+
+	// 3. data type
+	// int, float64, bool, string, rune
+	// Default type 0, 0.0, false, ""
+	p1(reflect.TypeOf(25))
+	p1(reflect.TypeOf(3.14))
+	p1(reflect.TypeOf(true))
+	p1(reflect.TypeOf("jhello"))
+	p1(reflect.TypeOf('😍'))
+	cV1 := 1.5
+	cV2 := int(cV1)
+	p1(cV2)
 }
